799242e5547fe0efdb79d38d4d3ef349e54548b9
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2021-06-21 19:08:51 +0530

    Create Maven.groovy

---
+++ vars/Maven.groovy
@@ -0,0 +1,16 @@
+def cal(body) {
+    def config = [:]
+    body.resolveStrategy = clousre.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def mavenGoals = config.mavenGoals
+    def branchName = config.branchName
+    def pomLocationName = config.pomLocation
+
+    /*docker.image('').inside('--net=host -v /app/maven:/app/maven')
+    {
+
+    }
+    */
+    sh 'mvn ${mavenGoals}'
+}
