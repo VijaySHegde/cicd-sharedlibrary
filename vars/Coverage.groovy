de476fec8008a4042a599454f892b3b4288a0c0d
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2021-06-23 12:01:33 +0530

    Update Coverage.groovy

--- vars/Coverage.groovy
+++ vars/Coverage.groovy
@@ -1,1 +1,14 @@
+def cal(body) {
+    def config = [:]
+    body.resolveStrategy = clousre.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
+    def junitResultLocation = config.junitResultLocation
+    def classPattern = config.classPattern
+    def execPattern = config.execPattern
+    def sourceInclusionPattern = config.sourceInclusionPattern
+    def sourcePattern = config.sourcePattern
+
+    jacoco classPattern: "${classPattern}", execPattern: "${execPattern}", sourceInclusionPattern: "${sourceInclusionPattern}", sourcePattern: "${sourcePattern}"
+}
